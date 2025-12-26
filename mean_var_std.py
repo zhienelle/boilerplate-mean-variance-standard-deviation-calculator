e3b30074f9cfbeaa6d7628a8a030a43d8311cee0
zhienelle <karylezhienelle.baylon.cics@ust.edu.ph>
2025-12-27 01:21:17 +0800

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def calculate(list):
     if len(list) != 9:
-        raise ValueError("Please enter exactly 9 integers separated by commas.")
+        raise ValueError("List must contain nine numbers.")
 
     a = np.array(list).reshape((3, 3))
 
